3fabcb24c01d6903a2456daa3b9996c9afa8e335
Walter Pereira de Souza Junior <walter.souza@iatec.com>
2021-05-16 20:14:38 -0300

    Products management implemented

---
+++ src/StockManagement.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace StockManagement.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; private set; }
+        public decimal CostPrice { get; private set; }
+
+        public Product(string name, decimal costPrice)
+        {
+            Name = name;
+            CostPrice = costPrice;
+        }
+
+        public void Update(string name, decimal costPrice)
+        {
+            Name = name;
+            CostPrice = costPrice;
+        }
+    }
+}
---
+++ test/StockManagement.Domain.Tests/Entities/ProductTests.cs
@@ -0,0 +1,22 @@
+﻿using StockManagement.Domain.Entities;
+using Xunit;
+
+namespace StockManagement.Domain.Tests.Entities
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void UpdateShouldUpdatesProperties()
+        {
+            // Arrange
+            var product = new Product("My New Product", 100);
+
+            // Act
+            product.Update("My Updated Product", 150);
+
+            // Assert
+            Assert.Equal("My Updated Product", product.Name);
+            Assert.Equal(150, product.CostPrice);
+        }
+    }
+}
