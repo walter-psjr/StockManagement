b660bcd51544e116a051fa8b9bf3e3029e823699
Walter Pereira de Souza Junior <walter.souza@iatec.com>
2021-05-17 08:52:45 -0300

    Authentication implemented

---
+++ src/StockManagement.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockManagement.Api.Helpers;
+
+namespace StockManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly TokenHelper _tokenHelper;
+
+        public AuthenticationController(TokenHelper tokenHelper)
+        {
+            _tokenHelper = tokenHelper;
+        }
+
+        [HttpPost("token")]
+        public IActionResult GenerateToken()
+        {
+            var token = _tokenHelper.GenerateAccessToken();
+
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ src/StockManagement.Api/Helpers/TokenHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace StockManagement.Api.Helpers
+{
+    public class TokenHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("AppSettings:Secret"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
