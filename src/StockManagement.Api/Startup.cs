7658e063f8c6b5e27b70c8019136344a37ad6e8a
Walter Pereira de Souza Junior <walter.souza@iatec.com>
2021-05-17 00:26:45 -0300

    Ef with integration test

--- src/StockManagement.Api/Startup.cs
+++ src/StockManagement.Api/Startup.cs
@@ -1,16 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using StockManagement.Domain.Interfaces.Repositories;
+using StockManagement.Infrastructure;
+using StockManagement.Infrastructure.Repositories;
 
 namespace StockManagement.Api
 {
@@ -26,12 +23,17 @@ namespace StockManagement.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StockManagement.Api", Version = "v1" });
             });
+
+            services.AddDbContext<StockManagementContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("PostgreSQL")));
+
+            services.AddScoped<IStoreRepository, StoreRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/StockManagement.Infrastructure/Repositories/BaseEfRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManagement.Domain.Interfaces.Repositories;
+using System;
+using System.Threading.Tasks;
+using StockManagement.Domain.Entities;
+
+namespace StockManagement.Infrastructure.Repositories
+{
+    public class BaseEfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly StockManagementContext _context;
+
+        public BaseEfRepository(StockManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<T> SaveAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
